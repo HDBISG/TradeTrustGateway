61678d7fef7e6d4499b7adbcd21d1f54e74d7fb4
ZhangJi <zhangji@hotmail.com>
2020-08-28 17:42:41 +0800

    issue

---
+++ test/testIssue.ts
@@ -0,0 +1,3 @@
+import issueComponent  from "../src/implementations/issueComponent";
+
+issueComponent("./resource/wallet.json","abc","0x1edfEa4A2205E85031d5A8a346386dd6921ff9bB","0xbebefbbe2888fff27c2ff3aced6bf527338ea9e0b4685de25f417a0efb6cf53b" );
