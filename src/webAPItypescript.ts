3613c432b9ee2e50e26d8e6b626640ae846c37b3
ZhangJi <zhangji@hotmail.com>
2020-09-07 15:03:18 +0800

    core

--- src/webAPItypescript.ts
+++ src/webAPItypescript.ts
@@ -35,16 +35,8 @@ import {
 
 var app = express();
 app.use(bodyParser.json());
-
-app.all('*', function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Content-Type,Content-Length, Authorization, Accept,X-Requested-With");
-  res.header("Access-Control-Allow-Methods","PUT,POST,GET,DELETE,OPTIONS");
-  res.header("X-Powered-By",' 3.2.1')
-  // if(req.method=="OPTIONS") res.send(200);
-  // else  next();
-});
-
+var cors = require('cors');
+app.use(cors());
 
 var tradeTrustService = new TradeTrustService();
 
