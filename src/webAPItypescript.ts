a71e03c9c78a191e0b221bc1823a195be630d061
ZhangJi <zhangji@hotmail.com>
2020-09-07 14:53:47 +0800

    security

--- src/webAPItypescript.ts
+++ src/webAPItypescript.ts
@@ -36,6 +36,16 @@ import {
 var app = express();
 app.use(bodyParser.json());
 
+app.all('*', function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Content-Type,Content-Length, Authorization, Accept,X-Requested-With");
+  res.header("Access-Control-Allow-Methods","PUT,POST,GET,DELETE,OPTIONS");
+  res.header("X-Powered-By",' 3.2.1')
+  // if(req.method=="OPTIONS") res.send(200);
+  // else  next();
+});
+
+
 var tradeTrustService = new TradeTrustService();
 
 app.post("/createWallet", async function (req: Request, res: Response) {
